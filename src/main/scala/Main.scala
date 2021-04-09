f1a1de332a2dfa86888ba73d261a2bbc17452ca6
Luis Hankel <kaes3kuch3n@gmail.com>
2021-04-09 15:54:29 +0200

    Added test case and hello world example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,3 @@
 object Main extends App {
-// test
+  println("Hello, world!")
 }
--- src/test/scala/TestSomething.scala
+++ src/test/scala/TestSomething.scala
@@ -1,7 +1,8 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.{convertToAnyShouldWrapper, equal}
 
 class TestSomething extends AnyFlatSpec{
-  "A test" should "fail" in {
-
+  "1 plus 1" should "equal 2" in {
+    1 + 1 should equal (2)
   }
 }
