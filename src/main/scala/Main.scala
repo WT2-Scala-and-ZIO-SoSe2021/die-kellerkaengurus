c5ef9ba0a250a02e6267dd244fd9db70fe095d11
Josephine Battiston <josephinebattiston@web.de>
2021-04-16 14:12:19 +0200

    added task 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,14 @@
 object Main extends App {
   println("Hello, world!")
+
+  // TODO: TASK 1
+  def max(arr: Array[Int]): Int = arr.reduce((x, y) => x max y)
+  def min(arr: Array[Int]): Int = arr.reduce((x,y) => x min y)
+  def sum(arr: Array[Int]): Int = arr.reduce((x,y) => x + y)
+
+  val x = Array(1,2,3)
+  val sumVal = sum(x)
+  print(sumVal)
+
+  // TODO: TASK 2
 }
