7360896f5d79c2d5d6f0e61cb908db24f250bbe7
Josephine Battiston <josephinebattiston@web.de>
2021-04-30 13:20:53 +0200

    removed double main and tests :D

--- src/test/scala/TestSomething.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.{convertToAnyShouldWrapper, equal}
-
-class TestSomething extends AnyFlatSpec{
-  "1 plus 1" should "equal 2" in {
-    1 + 1 should equal (2)
-  }
-}
